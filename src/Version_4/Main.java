5d356dea66fd30dc85ff26cbf52a2c15e8401b43
sathviksimhap <sathviksimhap@gmail.com>
2025-07-08 15:58:28 +0530

    v4 mvp done for real this time, last time was a fake

--- src/Version_4/Main.java
+++ src/Version_4/Main.java
@@ -4,25 +4,45 @@ import Base.Solution;
 import java.util.*;
 
 public class Main {
+
     public static List<List<Integer>> solution = new ArrayList<>();
     public static long paths = 0L, solutions = 0L;
+
     public static void main(String[] args){
+
         char[][] words_array = Solution.getArray();
-        int[] bitmask_set = getBitmaskSet(words_array);
+        HashMap<Integer, char[]> bitmask_to_word = getBitmaskMap(words_array);
+
+        int[] bitmask_set = new int[bitmask_to_word.size()];
+        int k=0;
+        for(int i: bitmask_to_word.keySet())
+            bitmask_set[k++] = i;
+
         BitSet[] graph = getGraph(bitmask_set);
         solve(graph);
+
+        //temp output
+        for(List<Integer> sol: solution){
+            for(int s: sol)
+                System.out.print(Arrays.toString(bitmask_to_word.get(bitmask_set[s])) + " ");
+            System.out.println();
+        }
     }
     public static void solve(BitSet[] graph){
         for(int u=0; u<graph.length; u++){
             List<Integer> clique = new ArrayList<>();
             clique.add(u);
-            BitSet cand = graph[u];
+
+            BitSet cand = (BitSet) graph[u].clone();
+
             findClique(clique, cand, graph);
         }
     }
     public static void findClique(List<Integer> clique, BitSet cand, BitSet[] graph){
+
+//        if(clique.size() == 2) System.out.println(2);
         if(clique.size()==5){
-            solution.add(clique);
+            solution.add(new ArrayList<>(clique));
             System.out.println("Solutions found: " + ++solutions);
             return;
         }
@@ -31,33 +51,30 @@ public class Main {
             System.out.println("Paths tried: " + paths);
 
         for(int v=cand.nextSetBit(0); v>=0; v=cand.nextSetBit(v+1)){
-            if(canAdd(clique, v, graph)){
-                clique.add(v);
-                BitSet new_cand = (BitSet) cand.clone();
-                new_cand.and(graph[v]);
-                findClique(clique, new_cand, graph);
-                clique.removeLast();
-            }
+            List<Integer> new_clique = new ArrayList<>(clique);
+            new_clique.add(v);
+
+            BitSet new_cand = (BitSet) cand.clone();
+            new_cand.and(graph[v]);
+
+            findClique(new_clique, new_cand, graph);
         }
     }
     public static boolean canAdd(List<Integer> clique, int v, BitSet[] graph){
         for(int u: clique)
-            if(!(graph[u].get(v))) return false;
+            if(!graph[u].get(v)) return false;
         return true;
     }
     //pre processing functions
-    public static int[] getBitmaskSet(char[][] words_array){
-        HashSet<Integer> set = new HashSet<>();
+    public static HashMap<Integer, char[]> getBitmaskMap(char[][] words_array){
+        HashMap<Integer, char[]> ans = new HashMap<>();
         for(char[] word: words_array){
             int bitmask=0;
-            for(char c: word) bitmask |= 1 << c - 'a';
-            set.add(bitmask);
+            for(char c: word) bitmask |= 1<<(c-'a');
+            if(!ans.containsKey(bitmask))
+                ans.put(bitmask, word);
         }
-        int[] arr = new int[set.size()];
-        int k=0;
-        for(int i: set) arr[k++] = i;
-
-        return arr;
+        return ans;
     }
     public static BitSet[] getGraph(int[] bitmask_set){
         int l = bitmask_set.length;
@@ -74,6 +91,9 @@ public class Main {
     }
 }
 /*
+
+[3058, 3064, 3316, 4275, 5337]
+
         base alg:
         func search(clique, cands):
             for n in cands
